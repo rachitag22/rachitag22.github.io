1e4790b44bb002aa6901e908540645f40add1fc0
Rachit Agarwal <rachitagarwal0202@gmail.com>
2017-02-13 22:10:49 -0500

    fixesgit add .!

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-getGradients();
+mainGradient();
 
-function getGradients() {
+function mainGradient() {
 	var gradientJSON = "https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json";
 	$.getJSON(gradientJSON, function(json) {
 		var gradientNum = Math.floor(Math.random() * json.length);
@@ -21,4 +21,11 @@ function getGradients() {
 		header.css("background", gradient);
 	});
 
+}
+
+function convertHex(hex){
+    hex = hex.replace('#','');
+    r = parseInt(hex.substring(0,2), 16);
+    g = parseInt(hex.substring(2,4), 16);
+    b = parseInt(hex.substring(4,6), 16);
 }
