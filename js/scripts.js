274f2a90d94fe596596efe19389e0dfa09853b1e
Rachit Agarwal <rachitagarwal0202@gmail.com>
2017-02-13 19:24:46 -0500

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
-var gradientJSON = "https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json";
+getGradients();
 
-$.getJSON(gradientJSON, function(json) {
+function getGradients() {
+	var gradientJSON = "https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json";
+	$.getJSON(gradientJSON, changeGradient(json));
+}
 
-});
+function changeGradient(json) {
+	console.log(json);
+}
