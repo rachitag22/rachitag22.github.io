0f293583f3109adf43d03c3fa7b32e53d63f5229
Rachit Agarwal <rachitagarwal0202@gmail.com>
2017-02-13 20:22:24 -0500

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,23 @@ getGradients();
 
 function getGradients() {
 	var gradientJSON = "https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json";
-	$.getJSON(gradientJSON, changeGradient(json));
-}
+	$.getJSON(gradientJSON, function(json) {
+		var gradientNum = Math.floor(Math.random() * json.length);
+		var colors = json[gradientNum]["colors"];
+		var color1 = colors[0];
+		var color2 = colors[1];
+		var direction = "bottom";
 
-function changeGradient(json) {
-	console.log(json);
+		var header = $(".main-header");
+
+		var gradient = color1;
+		header.css("background", gradient);
+
+		gradient = "-webkit-linear-gradient(to " + direction + ", " + color1 + ", " + color2 + ")";
+		header.css("background", gradient);
+		
+		gradient = "linear-gradient(to " + direction + ", " + color1 + ", " + color2 + ")";
+		header.css("background", gradient);
+	});
+
 }
